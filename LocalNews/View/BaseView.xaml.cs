3e73c32254c568902263096472ac8a162f5ac280
mickeypanda <pk.panda7381@gmail.com>
2020-09-26 12:21:23 +0530

    Base View and ViewModel added

---
+++ LocalNews/View/BaseView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalNews.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BaseView : ContentPage
+    {
+        public BaseView()
+        {
+            InitializeComponent();
+        }
+    }
+}
