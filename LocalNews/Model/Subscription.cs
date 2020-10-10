db590033d5d63844fc319127dffba7af8e302246
mickeypanda <pk.panda7381@gmail.com>
2020-10-11 02:26:19 +0530

    ++Subscription model

---
+++ LocalNews/Model/Subscription.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalNews.Model
+{
+    public class Subscription
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime SubscriptionDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
