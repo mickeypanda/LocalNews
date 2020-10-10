80d724f8288f6841fa727f4fafc27445fd1aac8a
mickeypanda <pk.panda7381@gmail.com>
2020-10-10 21:32:40 +0530

    added IAuth interface and android dependency services for Auth

---
+++ LocalNews/Helpers/AuthHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalNews.Helpers
+{
+    public interface IAuth
+    {
+        Task<bool> RegisterUser(string name, string email, string password);
+        Task<bool> AuthenticateUser(string email, string password);
+        bool IsAuthenticated();
+        string GetCurrentUserID();
+    }
+
+    public class Auth
+    {
+        private static IAuth auth;
+        public static async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            return await auth.RegisterUser(name, email, password);
+        }
+        public static async Task<bool> AuthenticateUser(string email, string password)
+        {
+            return await auth.AuthenticateUser(email, password);
+        }
+        public static bool IsAuthenticated()
+        {
+            return auth.IsAuthenticated();
+        }
+        public static string GetCurrentUserID()
+        {
+            return auth.GetCurrentUserID();
+        }
+    }
+}
