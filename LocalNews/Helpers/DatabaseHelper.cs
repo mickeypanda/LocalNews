4a684cdcdb1905b60e6de8ff1582de2c7875d36a
mickeypanda <pk.panda7381@gmail.com>
2020-10-11 02:39:37 +0530

    ++DatabaseHelper and IFirestore interface

---
+++ LocalNews/Helpers/DatabaseHelper.cs
@@ -0,0 +1,41 @@
+﻿using LocalNews.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalNews.Helpers
+{
+    public interface IFirestore
+    {
+        Task<bool> InsertSubscription(Subscription subscription);
+        Task<bool> DeleteSubscription(Subscription subscription);
+        Task<bool> UpdateSubscription(Subscription subscription);
+        Task<IList<Subscription>> ReadSubscriptions();
+
+    }
+    public class DatabaseHelper
+    {
+        private IFirestore firestore;
+        public Task<bool> DeleteSubscription(Subscription subscription)
+        {
+            return firestore.DeleteSubscription(subscription);
+        }
+
+        public Task<bool> InsertSubscription(Subscription subscription)
+        {
+            return firestore.InsertSubscription(subscription);
+        }
+
+        public Task<IList<Subscription>> ReadSubscriptions()
+        {
+            return firestore.ReadSubscriptions();
+        }
+
+        public Task<bool> UpdateSubscription(Subscription subscription)
+        {
+            return firestore.UpdateSubscription(subscription);
+        }
+    }
+}
