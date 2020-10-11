a62ee5b9adbff9a64f11a19b338121fdb9244df7
mickeypanda <pk.panda7381@gmail.com>
2020-10-11 19:39:46 +0530

    ++SubscriptionsViewModel, created a list view for the SubscriptionsView

---
+++ LocalNews/ViewModel/SubscriptionsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using LocalNews.Helpers;
+using LocalNews.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace LocalNews.ViewModel
+{
+    public class SubscriptionsViewModel:BaseViewModel
+    {
+        private ObservableCollection<Subscription> subscriptions;
+
+        public ObservableCollection<Subscription> Subscriptions
+        {
+            get { return subscriptions; }
+            set 
+            { 
+                subscriptions = value;
+                OnPropertyChanged("Subscriptions");
+            }
+        }
+
+        public SubscriptionsViewModel()
+        {
+            Subscriptions = new ObservableCollection<Subscription>();
+        }
+
+        public async void AssignSubscriptions()
+        {
+            var subscriptions = await DatabaseHelper.ReadSubscriptions();
+            if (subscriptions != null)
+            {
+                Subscriptions.Clear();
+                foreach (Subscription s in subscriptions)
+                    Subscriptions.Add(s);
+            }
+        }
+    }
+}
